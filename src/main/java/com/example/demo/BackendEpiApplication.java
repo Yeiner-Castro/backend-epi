fa3af8d400f3e103da2ad071ef4a012ece049bd8
Yeiner-Castro <yeiner.castro@unillanos.edu.co>
2024-07-17 20:50:04 -0500

    Add-Dependencias

---
+++ src/main/java/com/example/demo/BackendEpiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEpiApplication.class, args);
+	}
+
+}
